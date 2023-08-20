a27dca6d3a4cf5e5160f210faaea1366eeeb8d8b
Bipon Roy <biponroy400@gmail.com>
2023-08-21 02:07:47 +0600

    updated cart functionality

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 let total = 0;
+let floatTotal = 0;
 let discount = 0;
 function cartCalculation(target) {
+    //getting product name
     const itemContainer = document.getElementById("cart-list");
     const itemName = target.childNodes[3].childNodes[3].innerText;
+
+    //create list of product name and adding to the container
     const li = document.createElement("li");
     li.innerText = itemName;
     itemContainer.appendChild(li);
 
+    //getting product name from container
     const price = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
+
+    //calculate total price
     total = parseFloat(total) + parseFloat(price);
     floatTotal = total.toFixed(2);
 
+    //set total price value to total value
     const grandTotal = document.getElementById("grand-total");
     grandTotal.innerText = floatTotal;
 
+    //activating Make-Purchase button
     const discountValue = document.getElementById("discount");
 
     const makePurchase = document.getElementById("btn-purchase");
@@ -23,6 +32,7 @@ function cartCalculation(target) {
         makePurchase.classList.add("bg-main-color");
     }
 
+    //activating apply button and updating discount value
     const applyButton = document.getElementById("btn-discount");
 
     if (floatTotal >= 200) {
@@ -32,17 +42,21 @@ function cartCalculation(target) {
         applyButton.classList.add("bg-main-color");
         newDiscount = floatTotal * 0.2;
         discount = newDiscount.toFixed(2);
-        discountValue.innerText = discount;
     }
 
+    discountValue.innerText = "0.00";
+    //added apply button functionality with promo code
     applyButton.addEventListener("click", function () {
         const discountField = document.getElementById("discount-field");
         let totalAfterDiscount = 0;
+        //calculating total price after applying promo code
         if (discountField.value === "SELL200") {
+            discountValue.innerText = discount;
             totalAfterDiscount = floatTotal - discount;
             grandTotal.innerText = totalAfterDiscount.toFixed(2);
             discountField.value = "";
         }
     });
+    // update total price after discount
     document.getElementById("total").innerText = floatTotal;
 }
